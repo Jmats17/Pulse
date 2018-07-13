c5e206d97348d270b74ef22e3819d83fc12ba8b2
Justin Matsnev <jmatsnev@gmail.com>
2018-07-12 21:25:51 -0400

    ideas and qa done

--- Podfile
+++ Podfile
@@ -5,7 +5,7 @@ target 'Pulse' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
 pod 'PinterestSegment'
-
+pod 'DropDown'
   # Pods for Pulse
 
 
---
+++ Pulse/View Controllers/IdeaPitchViewController.swift
@@ -0,0 +1,140 @@
+//
+//  IdeaPitchViewController.swift
+//  Pulse
+//
+//  Created by Justin Matsnev on 7/12/18.
+//  Copyright © 2018 Justin Matsnev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import DropDown
+
+class IdeaPitchViewController : UIViewController {
+    
+    @IBOutlet weak var privateSwitch : AnimatedSwitch!
+    @IBOutlet weak var titleField : UITextField!
+    @IBOutlet weak var ideaField : UITextView!
+    @IBOutlet weak var ideaPictureField : UIImageView!
+    @IBOutlet weak var privateLabel : UILabel!
+    @IBOutlet weak var categoryLabel : UILabel!
+    @IBOutlet weak var dropDownView : UIView!
+    @IBOutlet weak var submitButton : UIButton!
+
+    let animatedSwitch = AnimatedSwitch()
+    var isAnimated = true
+    let dropDown = DropDown()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleField.delegate = self
+        ideaField.delegate = self
+        
+        submitButton.layer.cornerRadius = 5.0
+        
+        dropDownView.layer.cornerRadius = 5.0
+        dropDownView.layer.backgroundColor = UIColor(red: 236/255, green: 240/255, blue: 241/255, alpha: 1.0).cgColor
+        dropDown.anchorView = dropDownView
+        dropDown.dataSource = ["Team Building", "Cost Cutting", "Product", "Onboarding", "Office", "Other"]
+        
+        styleTextFieldTextView()
+        privateSwitch.animationDidStart = {
+            if self.isAnimated {
+                self.ideaPictureField.image = #imageLiteral(resourceName: "Lightbulbwhite")
+                self.titleField.layer.backgroundColor = UIColor.white.cgColor
+                self.privateLabel.textColor = UIColor.white
+                self.submitButton.layer.backgroundColor = UIColor.white.cgColor
+                self.submitButton.setTitleColor(UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0), for: .normal)
+                self.isAnimated = false
+            }
+            else {
+                self.ideaPictureField.image = #imageLiteral(resourceName: "Lightbulb")
+                self.privateLabel.textColor = UIColor.lightGray
+                self.ideaField.textColor = UIColor.black
+                self.titleField.textColor = UIColor.black
+                self.submitButton.layer.backgroundColor = UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0).cgColor
+                self.submitButton.setTitleColor(UIColor.white, for: .normal)
+                self.isAnimated = true
+            }
+        }
+
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let firstTouch = touches.first {
+            let dropDownTapped = self.view.hitTest(firstTouch.location(in: self.view), with: event)
+            
+            if dropDownTapped === dropDownView {
+                print("touch is inside")
+                dropDown.bottomOffset = CGPoint(x: 0, y:(dropDown.anchorView?.plainView.bounds.height)!)
+
+                self.dropDown.show()
+                self.dropDown.direction = .bottom
+                dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
+                    self.categoryLabel.text = item
+                    print("Selected item: \(item) at index: \(index)")
+                }
+                
+            } else {
+                print("touch is outside")
+            }
+        }
+    }
+    
+    @IBAction func submit(sender : UIButton) {
+        
+    }
+    
+    func styleTextFieldTextView() {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: titleField.frame.height))
+        titleField.leftView = paddingView
+        titleField.leftViewMode = UITextFieldViewMode.always
+        titleField.layer.borderWidth = 1.0
+        titleField.layer.borderColor = UIColor(red: 236/255, green: 240/255, blue: 241/255, alpha: 1.0).cgColor
+        titleField.layer.cornerRadius = 5.0
+        
+        ideaField.text = "Explain your idea here"
+        ideaField.textColor = UIColor(red: 154/255, green: 154/255, blue: 154/255, alpha: 0.5)
+        ideaField.layer.borderWidth = 1.0
+        ideaField.layer.borderColor = UIColor(red: 236/255, green: 240/255, blue: 241/255, alpha: 1.0).cgColor
+        ideaField.layer.cornerRadius = 5.0
+    }
+    
+    
+}
+
+extension IdeaPitchViewController : UITextFieldDelegate, UITextViewDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.titleField.resignFirstResponder()
+        return true
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        ideaField.text = ""
+        ideaField.textColor = UIColor.black
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if(text == "\n")
+        {
+            view.endEditing(true)
+            return false
+        }
+        else
+        {
+            return true
+        }
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
